b2134bfda48b19a185fe6f8cf34204c2b4c22e13
sametweb <sametmutevellioglu@gmail.com>
2020-09-30 01:26:41 -0700

    removed let's encrypt

--- index.js
+++ index.js
@@ -48,18 +48,3 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log("server is working on 4000");
 });
-
-require("greenlock-express")
-  .init({
-    packageRoot: __dirname,
-    configDir: "./greenlock.d",
-
-    // contact for security and critical bug notices
-    maintainerEmail: "sametmutevellioglu@gmail.com",
-
-    // whether or not to run at cloudscale
-    cluster: false,
-  })
-  // Serves on 80 and 443
-  // Get's SSL certificates magically!
-  .serve(server);
