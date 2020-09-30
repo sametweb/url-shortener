0a911c7296c0f8aeea150aa7751eb2d1c8ddbd95
sametweb <sametmutevellioglu@gmail.com>
2020-09-30 01:05:35 -0700

    let's encrypt

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const Url = require("./database/url-model");
 server.use(express.json());
 server.use(cors());
 
+require("greenlock-express")
+  .init({
+    packageRoot: __dirname,
+    configDir: "./greenlock.d",
+
+    // contact for security and critical bug notices
+    maintainerEmail: "sametmutevellioglu@gmail.com",
+
+    // whether or not to run at cloudscale
+    cluster: false,
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
+
 server.get("/", (req, res) => {
   res.send("URL Shortener");
 });
