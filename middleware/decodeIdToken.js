c41f66c23ac84e7b38b081e1505332348f4ca981
sametweb <sametmutevellioglu@gmail.com>
2020-10-05 03:34:11 -0700

    headers auth

--- middleware/decodeIdToken.js
+++ middleware/decodeIdToken.js
@@ -3,11 +3,12 @@ admin.initializeApp();
 
 module.exports = async (req, _, next) => {
   try {
-    const idToken = req.headers.ID_TOKEN;
+    const idToken = req.headers.id_token;
     const decodedToken = await admin.auth().verifyIdToken(idToken);
     req.user_id = decodedToken.uid;
   } catch {
     req.user_id = 0;
   }
+  console.log("idtoken", req.headers.id_token);
   next();
 };
