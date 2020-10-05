1c23d3018931f5cd98876563b543b3bd9769fbb0
sametweb <sametmutevellioglu@gmail.com>
2020-10-04 23:37:52 -0700

    headers

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ const cache = {};
 
 server.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", [
-    "https://omiturl.com",
-    "https://ou.tc",
-  ]);
+  const allowedOrigins = ["https://omiturl.com", "https://ou.tc"];
+  const requestOrigin = req.headers.origin;
+  if (requestOrigin in allowedOrigins) {
+    res.setHeader("Access-Control-Allow-Origin", requestOrigin);
+  }
 
   // Request methods you wish to allow
   res.setHeader(
