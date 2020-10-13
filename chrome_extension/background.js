559c1db77ef10baaf90360b18e88b1a35d101c6f
sametweb <sametmutevellioglu@gmail.com>
2020-10-12 20:05:28 -0700

    browser extension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -37,3 +37,13 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.onClicked.addListener(getword);
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {
+      message: "clicked_browser_action",
+    });
+  });
+});
